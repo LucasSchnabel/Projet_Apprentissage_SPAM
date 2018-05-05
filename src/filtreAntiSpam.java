59ccbbbedc5a3027691abc1032e3f89ba61e1e74
Unknown <schnabel.lucas@gmail.com>
2018-05-05 18:20:11 +0200

    ajout fichier java

---
+++ src/filtreAntiSpam.java
@@ -0,0 +1,21 @@
+
+public class filtreAntiSpam {
+
+	private String[] dictionnaire;
+	
+	public static void main(String[] args){
+		
+	}
+	
+	public void charger_dictionnaire(String nomFichier){
+		//cette fonction doit pouvoir charger un dictionnaire (par
+		//exemple dans un tableau de mots) à partir d’un fichier texte
+	}
+	
+	public boolean[] lire_message(String nomFichier){
+		boolean[] res = null;
+		//cette fonction doit pouvoir lire un message (dans un fichier texte) et le
+		//traduire en une représentation sous forme de vecteur binaire x à partir d’un dictionnaire
+		return res;
+	}
+}
